03f7a17b440a0d19a7b8ba467039dc46c2d8cd83
Vira Murray <vira.avramenko@gmail.com>
2022-10-28 15:32:00 -0400

    Add project files.

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs= new List<DogViewModel>();
+        public IActionResult Index()
+        {
+
+            return View(dogs);
+        }
+        public string Hello()
+        {
+            return "Who's htere?";
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            //return View("Index");
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WebApplication1/Models/DogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class DogViewModel
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+}
